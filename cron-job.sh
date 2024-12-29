9cc8a5d8fb0806fd0edd68f5deab231d87d8ba32
Justin Morneau <jmorneau25@gmail.com>
2024-12-29 04:13:13 -0500

    Fix formatting in cron job script and add newline at end of file

--- cron-job.sh
+++ cron-job.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# Requête HTTP
+#Requête HTTP
 curl -X POST -H "Content-Type: application/json" -d '{"key": "96a0378cc7227e65dc344a7edb016d61380fbe5c4775f3f21028dbaf273ae644"}' http://10.0.0.201:3000/api/cron-job
+
+echo ""
